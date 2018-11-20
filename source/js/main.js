cc74f82718a721022f560f8e80104555c292a24c
Ulia Usalko <usalko.ulia@gmail.com>
2018-11-20 21:40:29 +0200

    Исправила неверную разметку галлереи

--- source/js/main.js
+++ source/js/main.js
@@ -9,16 +9,18 @@ var buttons_container = "reviews__buttons";
 
 // Add a function that generates pagination to prototype
 Siema.prototype.addPagination = function() {
-  for (let i = 0; i < this.innerElements.length; i++) {
-    const btn = document.createElement("button");
-    btn.classList.add(button_class);
-    if (i === 0) btn.classList.add(active_button_class);
-    btn.addEventListener("click", () => {
-      document.querySelector("." + active_button_class).classList.remove(active_button_class);
-      btn.classList.add(active_button_class);
-      this.goTo(i);
-    });
-    document.querySelector("." + buttons_container).appendChild(btn);
+  for (var i = 0; i < this.innerElements.length; i++) {
+    (function(i) {
+      var btn = document.createElement("button");
+      btn.classList.add(button_class);
+      if (i === 0) btn.classList.add(active_button_class);
+      btn.addEventListener("click", function() {
+        document.querySelector("." + active_button_class).classList.remove(active_button_class);
+        btn.classList.add(active_button_class);
+        mySiema.goTo(i);
+      });
+      document.querySelector("." + buttons_container).appendChild(btn);
+    })(i);
   }
 }
 
