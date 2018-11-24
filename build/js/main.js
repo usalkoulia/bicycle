119f522faab94f8c3ac01f1b358ad3c6ebebfed1
Ulia Usalko <usalko.ulia@gmail.com>
2018-11-24 17:54:51 +0200

    Работа над багом блока bikes

--- build/js/main.js
+++ build/js/main.js
@@ -1,11 +1,17 @@
 // Siema doesn't come with pagination built in
 // But it is very easy to add one if you want
 
-// New siema instance
-var mySiema = new Siema();
 var button_class = "reviews__button";
 var active_button_class = "reviews__button--active";
 var buttons_container = "reviews__buttons";
+
+// New siema instance
+var mySiema = new Siema({
+  onChange: function() {
+    document.querySelector("." + active_button_class).classList.remove(active_button_class);
+    document.querySelector("." + button_class + ":nth-child(" + (mySiema.currentSlide + 1) + ")").classList.add(active_button_class);
+  },
+});
 
 // Add a function that generates pagination to prototype
 Siema.prototype.addPagination = function() {
