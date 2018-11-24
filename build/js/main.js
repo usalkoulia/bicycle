1aebc9f514fae2fd1d2254ccfc5331a728f2f39d
Ulia Usalko <usalko.ulia@gmail.com>
2018-11-24 15:08:36 +0200

    delite build

---
+++ build/js/main.js
@@ -0,0 +1,28 @@
+// Siema doesn't come with pagination built in
+// But it is very easy to add one if you want
+
+// New siema instance
+var mySiema = new Siema();
+var button_class = "reviews__button";
+var active_button_class = "reviews__button--active";
+var buttons_container = "reviews__buttons";
+
+// Add a function that generates pagination to prototype
+Siema.prototype.addPagination = function() {
+  for (var i = 0; i < this.innerElements.length; i++) {
+    (function(i) {
+      var btn = document.createElement("button");
+      btn.classList.add(button_class);
+      if (i === 0) btn.classList.add(active_button_class);
+      btn.addEventListener("click", function() {
+        document.querySelector("." + active_button_class).classList.remove(active_button_class);
+        btn.classList.add(active_button_class);
+        mySiema.goTo(i);
+      });
+      document.querySelector("." + buttons_container).appendChild(btn);
+    })(i);
+  }
+}
+
+// Trigger pagination creator
+mySiema.addPagination();
